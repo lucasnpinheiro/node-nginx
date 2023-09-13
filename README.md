cda6335ad249233e5f24925c7405ab6cef5dcc86
Lucas Nunes Pinto Pinheiro <lucasnpinheiro>
2023-09-13 10:04:13 -0300

    Desafio Node

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# node-nginx
-Docker Node Nginx curso Fullcycle
+# BUILD
+
+### Build image golang.
+
+```bash
+
+   docker build -t lucasnpinheiro/node-nginx .
+
+```
+
+### List image.
+
+```bash
+
+   docker images
+
+```
+
+### Run image.
+
+```bash
+
+   docker run lucasnpinheiro/node-nginx
+
+```
+
+### Pull image.
+
+```bash
+
+   docker pull lucasnpinheiro/node-nginx
+
+```
+
+### Push image.
+
+```bash
+
+   docker push lucasnpinheiro/node-nginx
+
+```
+
+### Repository
+
+[node-nginx](https://github.com/lucasnpinheiro/node-nginx)
+
+### Up Server
+
+```bash
+
+   docker compose up -d --build
+
+```
+
+### Logs Server
+
+```bash
+
+   docker compose logs -f
+
+```
+
+### Down Server
+
+```bash
+
+   docker compose down
+
+```
+
+### Open sistem
+
+[localhost](http://localhost)
---
+++ node/index.js
@@ -0,0 +1,71 @@
+const mysql = require('mysql')
+const express = require('express')
+const { faker } = require('@faker-js/faker')
+
+const config = {
+   host: 'db',
+   user: 'root',
+   password: 'root',
+   database: 'node_db'
+}
+
+const app = express()
+const port = 3000
+
+const createTable = async () => {
+   const connection = await mysql.createConnection(config)
+
+   const sql = "CREATE TABLE IF NOT EXISTS people (id INT AUTO_INCREMENT, name VARCHAR(255), PRIMARY KEY (id))"
+   await connection.query(sql)
+
+   await connection.end()
+}
+
+const insert = async () => {
+   const connection = await mysql.createConnection(config)
+
+   const name = `${faker.person.fullName()}`
+   const sqlInsert = `INSERT INTO people(name) values('${name}')`
+
+   await connection.query(sqlInsert)
+
+   await connection.end()
+}
+
+const list = async () => {
+   const connection = await mysql.createConnection(config)
+   const data = await new Promise((res, rej) => {
+      connection.query('SELECT name FROM people ORDER BY name ASC', (err, results) => {
+         if (err) return rej(err);
+         res(results);
+      });
+   });
+
+   await connection.end();
+
+   return data;
+}
+
+createTable()
+insert()
+
+app.get('/', (req, res) => {
+   const print = [];
+   print.push('<h1>Full Cycle</h1>');
+
+   const result = list()
+
+   result.then((itens) => {
+      for (let item of itens) {
+         print.push(`<p>${item.name}</p>`)
+      }
+
+      res.send(print.join(''))
+   })
+
+})
+
+
+app.listen(port, () => {
+   console.log('Rodando na porta ' + port)
+})
